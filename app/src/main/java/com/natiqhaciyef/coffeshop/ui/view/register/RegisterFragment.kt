ac4e7808425da387877d007991184e4257dcb0fc
Natiq <natiq00h2272@gmail.com>
2023-02-10 17:23:57 +0400

    RegisterFragment.kt and fragment_register.xml created and implemented successfully

--- app/src/main/java/com/natiqhaciyef/coffeshop/ui/view/register/RegisterFragment.kt
+++ app/src/main/java/com/natiqhaciyef/coffeshop/ui/view/register/RegisterFragment.kt
@@ -1,6 +1,10 @@
 package com.natiqhaciyef.coffeshop.ui.view.register
 
+import android.os.Build
 import android.os.Bundle
+import android.text.Html
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -22,6 +26,27 @@ class RegisterFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setup()
+    }
 
+    private fun setup(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            binding.goToLogin.text =
+                Html.fromHtml(getString(R.string.go_to_login_text), Html.FROM_HTML_MODE_COMPACT)
+        } else {
+            binding.goToLogin.text = Html.fromHtml(getString(R.string.go_to_login_text))
+        }
+
+        binding.passwordVisibile.setOnClickListener {
+            binding.passwordTextInputRegister.transformationMethod = PasswordTransformationMethod.getInstance()
+            binding.passwordVisibile.visibility = View.GONE
+            binding.passwordVisibileOff.visibility = View.VISIBLE
+        }
+
+        binding.passwordVisibileOff.setOnClickListener {
+            binding.passwordTextInputRegister.transformationMethod = HideReturnsTransformationMethod.getInstance()
+            binding.passwordVisibile.visibility = View.VISIBLE
+            binding.passwordVisibileOff.visibility = View.GONE
+        }
     }
 }
