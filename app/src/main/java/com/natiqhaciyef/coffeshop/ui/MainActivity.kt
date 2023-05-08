c5bd2fe30802e7e5d4b793a110854439a11cc373
natiqhaciyef <natiq00h2272@gmail.com>
2023-05-08 20:56:33 +0400

    UI main activity changed

--- app/src/main/java/com/natiqhaciyef/coffeshop/ui/MainActivity.kt
+++ app/src/main/java/com/natiqhaciyef/coffeshop/ui/MainActivity.kt
@@ -11,6 +11,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+//    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
